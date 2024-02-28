ebf0d4a2179d6e6e31d32dbdce4b02edc37c0b32
Sercan <56640809+sercantrhnn@users.noreply.github.com>
2024-02-28 14:17:25 +0300

    Update firebase.js

--- MERN/Client/ete_technology/src/firebase.js
+++ MERN/Client/ete_technology/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCuYmBGFnptGaGhm0APLTZFIwa0TRNTVmw",
-  authDomain: "ete-technology.firebaseapp.com",
-  projectId: "ete-technology",
-  storageBucket: "ete-technology.appspot.com",
-  messagingSenderId: "792932722150",
-  appId: "1:792932722150:web:0a3d0dee97edfb8dcb6227"
+  apiKey: //",
+  authDomain: //,
+  projectId: //,
+  storageBucket: //,
+  messagingSenderId: //,
+  appId: //
 };
 
 export const app = initializeApp(firebaseConfig);
